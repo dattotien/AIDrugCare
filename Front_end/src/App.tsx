b2acf58e7a4be7f64a115ed616a5947a4b8ddd79
Datto <dattotienn@gmail.com>
2025-08-23 19:40:04 +0700

    Drug Interaction check frontend

--- Front_end/src/App.tsx
+++ Front_end/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type React from "react"
+import { useState } from "react"
+import { Breadcrumb, Layout, Menu } from "antd"
+const { Header, Content, Footer } = Layout
+import DrugInteractionChecker from "./Components/DrugInteractionChecker"
+import UETLogo from "./assets/UET.png";
+const items = [
+  { key: "1", label: "Khám chữa bệnh và kê đơn" },
+  { key: "2", label: "Hồ sơ" },
+  { key: "3", label: "Lịch sử khám bệnh" },
+  { key: "4", label: "Danh mục thuốc" },
+  { key: "5", label: "Kiểm tra tương tác thuốc" },
+]
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
+  const [selectedLabel, setSelectedLabel] = useState("");
+  const handleMenuSelect = ({ key }: { key: string }) => {
+  const selectedItem = items.find(item => item.key === key)
+  if (selectedItem) {
+    setSelectedLabel(selectedItem.label)
+  }
+}
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout className="layout" style={{ minHeight: "100vh", minWidth: "100vw" }}>
+      <Header style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo" style={{ marginRight: "10px", height: "100%", padding: "2px" }}>
+          <img src={UETLogo} alt="UET Logo" style={{ height: "100%" }} />
+        </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          onSelect={handleMenuSelect}
+          items={items}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
+      <Content style={{ padding: "0 48px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }} items={[{ title: "Trang chủ" }, { title: selectedLabel }]} />
+        <div
+          style={{
+            background: "#fff",
+            minHeight: "70vh",
+            padding: "10vh",
+            borderRadius: 20,
+          }}
+        >
+        {
+          selectedLabel === "Kiểm tra tương tác thuốc" && <DrugInteractionChecker />
+        }
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>Hệ thống hỗ trợ kê đơn thuốc - Đại học Công nghệ - ĐHQGHN</Footer>
+    </Layout>
   )
 }
+
+
 
 export default App
