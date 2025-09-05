bd563243caa5978222cea15135b1304a26ff7cbb
thuwng <122499901+thuwng@users.noreply.github.com>
2025-09-05 16:13:37 +0700

    Fix

--- Front_end/src/App.tsx
+++ Front_end/src/App.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 import "./App.css";
-import DoctorScene from "./Components/DoctorScene/DoctorScene";
+import DoctorScene from "./Components/DoctorScene/DoctorScene.tsx";
 import PatientLoginScence from "./Components/PatientLoginScene/PatientLoginScene";
 const App: React.FC = () => {
   return <DoctorScene />;
