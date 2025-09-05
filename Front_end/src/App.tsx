4fbe6b9e56a3fcce426ad5346a59fe8bd362e75a
thuwng <122499901+thuwng@users.noreply.github.com>
2025-09-05 16:09:51 +0700

    Update App.tsx

--- Front_end/src/App.tsx
+++ Front_end/src/App.tsx
@@ -1,10 +1,6 @@
 import type React from "react";
 import "./App.css";
-<<<<<<< Updated upstream
 import DoctorScene from "./Components/DoctorScene/DoctorScene";
-=======
-import DoctorScene from "./Components/DoctorScene"
->>>>>>> Stashed changes
 import PatientLoginScence from "./Components/PatientLoginScene/PatientLoginScene";
 const App: React.FC = () => {
   return <DoctorScene />;
