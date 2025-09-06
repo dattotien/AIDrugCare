5ed825600d3f801662092bcdbc5ef2a6c93afd8d
Hoang Anh <181938134+nhhaanh2312@users.noreply.github.com>
2025-09-06 18:04:37 +0700

    Update App.tsx

--- Front_end/src/App.tsx
+++ Front_end/src/App.tsx
@@ -1,9 +1,20 @@
 import type React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DoctorLoginScene from "./Components/DoctorLoginScene/DoctorLoginScene.tsx";
 import DoctorScene from "./Components/DoctorScene/DoctorScene.tsx";
 import PatientLoginScence from "./Components/PatientLoginScene/PatientLoginScene";
+import VisitInfor from "./Components/Visit/VisitInfor.tsx";
 const App: React.FC = () => {
-  return <DoctorScene></DoctorScene>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<DoctorLoginScene />} />
+        <Route path="/dashboard/" element={<DoctorScene />} />
+        <Route path="/visit-info" element={<VisitInfor />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
