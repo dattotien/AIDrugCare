c655b063fac2751b157dafc4cbc20e62254dae6a
thuwng <122499901+thuwng@users.noreply.github.com>
2025-09-07 18:00:12 +0700

    Update DrugBank

---
+++ Front_end/src/Components/DrugBank/drug.types.ts
@@ -0,0 +1,27 @@
+export interface BrandName {
+  name: string;
+  route?: string;
+  strength?: string;
+  dosage_form?: string;
+  country?: string;
+}
+
+export interface DrugInteraction {
+  drugbank_id: string;
+  name: string;
+}
+
+export interface Drug {
+  _id: string;
+  generic_name: string;
+  description?: string;
+  brand_names?: BrandName[];
+  manufacturers?: string[];
+  dosage_forms?: string[];
+  categories?: string[];
+  atc_code?: string[];
+  chemical_formula?: string;
+  molecular_formula?: string;
+  drug_interaction?: DrugInteraction[];
+  synonyms?: string[];
+}
